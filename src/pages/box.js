37912c74ed172c3fef51ce3cfd37c9bf9c3d80ac
KEYUNG KIM <ku.kim@pmgrow.co.kr>
2019-07-09 15:46:33 +0900

    idea 프로젝트 설정 파일 추가, Box 컴포넌트 예제 추가

--- src/pages/box.js
+++ src/pages/box.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+// { Button } 포맷으로 import 하면, Box 속성이 적용안됨.
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Avatar from '@material-ui/core/Avatar'
 import Typography from '@material-ui/core/Typography'
 import SignalWifiOffIcon from '@material-ui/icons/SignalWifiOff'
+// The CSS specificity relies on the import order.
+// If you want the guarantee that the wrapped component's style will be overridden,
+// you need to import the Box last.
 import Box from '@material-ui/core/Box'
 
 const useStyles = makeStyles(theme => ({
@@ -51,7 +55,6 @@ const Page = () => {
 
       <Box clone pt={2} pr={1} pb={1} pl={2}>
         <Paper elevation={10}>
-          페이퍼
           <Grid container alignItems={'center'} spacing={2} wrap={'nowrap'}>
             <Grid item>
               <Box bgcolor={'primary.main'} clone>
@@ -62,6 +65,11 @@ const Page = () => {
             </Grid>
             <Grid item>
               <Typography>You have lost connection to the internet. This app is offline.</Typography>
+            </Grid>
+          </Grid>
+          <Grid container justify={'flex-end'}>
+            <Grid item>
+              <Button color={'primary'}>Turn on wifi</Button>
             </Grid>
           </Grid>
         </Paper>
